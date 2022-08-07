19b2beaa6ca680000f858799ee0e5b4d923dc98a
zhangqian <zhangqian0120@foxmail.com>
2022-08-08 01:05:27 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+import Layout from '@/layout'
+
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
+  },
+
+  {
+    path: '/vacation',
+    component: Layout,
+    redirect: '/vacation/index',
+    name: 'Vacation',
+    meta: { title: 'Vacation', icon: 'el-icon-s-help' },
+    children: [
+      {
+        path: 'index',
+        name: 'VacationIndex',
+        component: () => import('@/views/vacation/index'),
+        meta: { title: 'Vacation', icon: 'table' }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
